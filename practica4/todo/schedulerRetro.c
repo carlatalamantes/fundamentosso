4b9619ff25822cee07f480be9de2b84f8e2e19ad
carlatalamantes <carlatalamantes@hotmail.com>
2022-06-11 11:51:17 -0500

    Finishing Retro

--- practica4/todo/schedulerRetro.c
+++ practica4/todo/schedulerRetro.c
@@ -1,65 +1,98 @@
 #include <scheduler.h>
 
-#define QUEUES 10
-
+// Max length of priority queues
+#define MAXQUEUES 10
 
 extern THANDLER threads[MAXTHREAD];
 extern int currthread;
 extern int blockevent;
 extern int unblockevent;
 
-//QUEUE ready[QUEUES];
-QUEUE ready;
+// Array of priority queues
+QUEUE ready[MAXQUEUES];
 QUEUE waitinginevent[MAXTHREAD];
 
-//int currprior;
+// Global variable for current priority
+int priority;
+// Global counter for threads
+int nthreads = 0;
 
 void scheduler(int arguments)
 {
-	int old,next;
-	int changethread=0;
-	int waitingthread=0;
-	int i;
-	int nextlevel;
-	
-	int event=arguments & 0xFF00;
-	int callingthread=arguments & 0xFF;
+	int old, next;
+	int changethread = 0;
+	int waitingthread = 0;
 
-	if(event==NEWTHREAD)
+	int event = arguments & 0xFF00;
+	int callingthread = arguments & 0xFF;
+
+	if (event == NEWTHREAD)
 	{
-		threads[callingthread].status=READY;
-		//_enqueue(&ready[0],callingthread);
-	}
-	
-	if(event==BLOCKTHREAD)
-	{
-
-		threads[callingthread].status=BLOCKED;
-		_enqueue(&waitinginevent[blockevent],callingthread);
-
-		changethread=1;
+		threads[callingthread].status = READY;
+		// Highest priority list
+		_enqueue(&ready[0], callingthread);
+		priority = 0;
+		// Increment thread counter by one
+		nthreads += 1;
 	}
 
-	if(event==ENDTHREAD)
+	if (event == BLOCKTHREAD)
 	{
-		threads[callingthread].status=END;
-		changethread=1;
-	}
-	
-	if(event==UNBLOCKTHREAD)
-	{
-			threads[callingthread].status=READY;
-			_enqueue(&ready,callingthread);
+		threads[callingthread].status = BLOCKED;
+		_enqueue(&waitinginevent[blockevent], callingthread);
+		changethread = 1;
 	}
 
-	
-	if(changethread)
+	if (event == ENDTHREAD)
 	{
-		old=currthread;
-		next=_dequeue(&ready);
-		
-		threads[next].status=RUNNING;
-		_swapthreads(old,next);
+		threads[callingthread].status = END;
+		changethread = 1;
 	}
 
+	if (event == UNBLOCKTHREAD)
+	{
+		threads[callingthread].status = READY;
+		// Always enqueues highest priority list
+		_enqueue(&ready[0], callingthread);
+	}
+
+	if (event == TIMER)
+	{
+		threads[currthread].status = READY;
+		/* Adding thread to the next ready queue as long as it's not 
+		the last level and the amount of threads is not one
+		*/
+		if (priority < MAXQUEUES - 1 && nthreads > 1)
+		{
+			_enqueue(&ready[priority + 1], currthread);
+		}
+		else
+		{
+			_enqueue(&ready[priority], currthread);
+		}
+		changethread = 1;
+	}
+
+	if (changethread)
+	{
+		// Loop variable
+		int i;
+
+		old = currthread;
+
+		// Checking if there's threads in each queue
+		for (i = 0; i < MAXQUEUES; i++)
+		{
+			if (!_emptyq(&ready[i]))
+			{
+				// Get thread out and put it in next
+				next = _dequeue(&ready[i]);
+				// Point current priority to this found thread
+				priority = i;
+				break;
+			}
+		}
+		threads[next].status = RUNNING;
+		_swapthreads(old, next);
+	}
 }
